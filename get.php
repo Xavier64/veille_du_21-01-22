ca5c21a632a87351f83355556b9ca726e44c6afd
Xavier64 <91951017+Xavier64@users.noreply.github.com>
2022-01-21 09:31:15 +0100

    Create get.php

---
+++ get.php
@@ -0,0 +1,13 @@
+<?php
+
+ echo "Bonjour Mr ";
+ echo $_GET["nom"]; 
+ echo " votre adresse e-mail est : ";
+ echo $_GET["email"];
+
+ var_dump($_GET);
+//  var_dump($_SERVER);
+
+ ?>
+
+ <!-- Les informations sont visbles pars tous dans l'URL!! -->
